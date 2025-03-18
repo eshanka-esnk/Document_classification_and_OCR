12a534ca6da64689266909c2d3cd2105eec34267
eshanka-esnk <eshanka@gmail.com>
2025-03-18 09:52:55 +0530

    addded ui

--- helpers/helper.py
+++ helpers/helper.py
@@ -1,3 +1,4 @@
+import re
 from venv import logger
 from pdf2image import convert_from_path
 from torchvision.utils import make_grid
@@ -10,6 +11,347 @@ import ftfy
 import io
 import json
 import sys
+
+#### Reused code from git repo provided
+
+#ADHAAR READ
+def adhaar_read_data(text):
+    res=text.split()
+    name = None
+    dob = None
+    adh = None
+    sex = None
+    add = None
+
+    nameline = []
+    dobline = []
+    addline = []
+    
+    text0 = []
+    text1 = []
+    text2 = []
+    lines = text.split('\n')
+    for lin in lines:
+        s = lin.strip()
+        s = lin.replace('\n','')
+        s = s.rstrip()
+        s = s.lstrip()
+        text1.append(s)
+
+    if 'female' in text.lower():
+        sex = "FEMALE"
+    else:
+        sex = "MALE"
+    
+    text1 = list(filter(None, text1))
+    text0 = text1[:]
+    
+    try:
+
+        # Cleaning first names
+        name = text0[0]
+        name = name.rstrip()
+        name = name.lstrip()
+        name = name.replace("8", "B")
+        name = name.replace("0", "D")
+        name = name.replace("6", "G")
+        name = name.replace("1", "I")
+        name = re.sub('[^a-zA-Z] +', ' ', name)
+
+        # Cleaning DOB
+        dob = text0[1][-10:]
+        dob = dob.rstrip()
+        dob = dob.lstrip()
+        dob = dob.replace('l', '/')
+        dob = dob.replace('L', '/')
+        dob = dob.replace('I', '/')
+        dob = dob.replace('i', '/')
+        dob = dob.replace('|', '/')
+        dob = dob.replace('\"', '/1')
+        dob = dob.replace(":","")
+        dob = dob.replace(" ", "")
+
+        # Cleaning Adhaar number details
+        aadhar_number=''
+        for word in res:
+            if len(word) == 4 and word.isdigit():
+                aadhar_number=aadhar_number  + word + ' '
+        if len(aadhar_number)>=14:
+            print("Aadhar number is :"+ aadhar_number)
+        else:
+            print("Aadhar number not read")
+        adh=aadhar_number
+
+        #cleaning address
+        text0 = findword(text1, ('Address|Adress|ddress|Addess|Addrss|Addres|Add|Ad|Location)$'))
+        addline = text0[0]
+        add = addline.rstrip()
+        add = add.lstrip()
+        add = add.replace(" ", "")
+        add = add.replace("\"", "")
+        add = add.replace(";", "")
+        add = add.replace("%", "L")
+    except:
+        pass
+    
+    
+    
+    data = {}
+    data['Name'] = name
+    data['DOB'] = dob
+    data['Document Number'] = adh
+    data['Sex'] = sex
+    data['Document Type'] = "PAN"
+    return data
+def findword(textlist, wordstring):
+    lineno = -1
+    for wordline in textlist:
+        xx = wordline.split( )
+        if ([w for w in xx if re.search(wordstring, w)]):
+            lineno = textlist.index(wordline)
+            textlist = textlist[lineno+1:]
+            return textlist
+    return textlist
+
+#PAN READ
+def pan_read_data(text):
+    name = None
+    fname = None
+    dob = None
+    pan = None
+    nameline = []
+    dobline = []
+    panline = []
+    text0 = []
+    text1 = []
+    text2 = []
+    lines = text.split('\n')
+    for lin in lines:
+        s = lin.strip()
+        s = lin.replace('\n','')
+        s = s.rstrip()
+        s = s.lstrip()
+        text1.append(s)
+    text1 = list(filter(None, text1))
+    lineno = 0
+    for wordline in text1:
+            xx = wordline.split('\n')
+            if ([w for w in xx if re.search('(INCOMETAXDEPARWENT|INCOME|TAX|GOW|GOVT|GOVERNMENT|OVERNMENT|VERNMENT|DEPARTMENT|EPARTMENT|PARTMENT|ARTMENT|INDIA|NDIA)$', w)]):
+                text1 = list(text1)
+                lineno = text1.index(wordline)
+                break
+    text0 = text1[lineno+1:]
+    try:
+            name = text0[0]
+            name = name.rstrip()
+            name = name.lstrip()
+            name = name.replace("8", "B")
+            name = name.replace("0", "D")
+            name = name.replace("6", "G")
+            name = name.replace("1", "I")
+            name = re.sub('[^a-zA-Z] +', ' ', name)
+            # Cleaning Father's name
+            fname = text0[1]
+            fname = fname.rstrip()
+            fname = fname.lstrip()
+            fname = fname.replace("8", "S")
+            fname = fname.replace("0", "O")
+            fname = fname.replace("6", "G")
+            fname = fname.replace("1", "I")
+            fname = fname.replace("\"", "A")
+            fname = re.sub('[^a-zA-Z] +', ' ', fname)
+            # Cleaning DOB
+            dob = text0[2][:10]
+            dob = dob.rstrip()
+            dob = dob.lstrip()
+            dob = dob.replace('l', '/')
+            dob = dob.replace('L', '/')
+            dob = dob.replace('I', '/')
+            dob = dob.replace('i', '/')
+            dob = dob.replace('|', '/')
+            dob = dob.replace('\"', '/1')
+            dob = dob.replace(" ", "")
+            # Cleaning PAN Card details
+            text0 = findword(text1, '(Pormanam|Number|umber|Account|ccount|count|Permanent|ermanent|manent|wumm)$')
+            panline = text0[0]
+            pan = panline.rstrip()
+            pan = pan.lstrip()
+            pan = pan.replace(" ", "")
+            pan = pan.replace("\"", "")
+            pan = pan.replace(";", "")
+            pan = pan.replace("%", "L")
+    except:
+            pass
+    data = {}
+    data['Name'] = name
+    data['Father Name'] = fname
+    data['DOB'] = dob
+    data['Document Number'] = pan
+    data['Document Type'] = "PAN"
+    return data
+
+def findword(textlist, wordstring):
+    lineno = -1
+    for wordline in textlist:
+        xx = wordline.split( )
+        if ([w for w in xx if re.search(wordstring, w)]):
+            lineno = textlist.index(wordline)
+            textlist = textlist[lineno+1:]
+            return textlist
+    return textlist
+
+# vOTER ID READ
+def voterid_read_data(text):
+    text1 = []
+    
+    full_name =""
+    elder_name =""
+    sex =""
+    dob =""
+    # Splitting the lines to sort the text paragraph wise
+    lines = text.split('\n')
+    for lin in lines:
+        s = lin.strip()
+        s = s.rstrip()
+        s = s.lstrip()
+        text1.append(s)
+        
+    # Finding the electors number 
+    voter_no = findword(text1, '(PASSPORT|CARD|IDENTITY CARD)$')
+    voter_no = voter_no[0]
+    voter_no = voter_no.replace(" ", "")
+    
+    lines = text
+        
+    for x in lines.split('\n'):
+        _ = x.split()
+        if ([w for w in _ if re.search("(Elector's|ELECTOR'S)$", w)]):    
+            person_name = x
+            person_name = person_name.split(':')[1].strip()
+            full_name = person_name
+                
+        # Finding the father/husband/mother name        
+        if ([w for w in _ if re.search("(Father's|Mother's|FATHER'S|MOTHER'S)$", w)]):
+            elder_name = x
+            elder_name = elder_name.split(':')[1].strip()
+                
+        # Finding the gender of the electoral candidate
+        if ([w for w in _ if re.search('(Male|MALE|male)$', w)]):
+            sex = "Male"
+        elif ([w for w in _ if re.search('(Female|FEMALE|female)$', w)]):
+            sex = "Female"
+                
+        # Finding the Date of Birth 
+        if ([w for w in _ if re.search('(Year|YEAR|Birth|Date|Date of Birth|DATE OF BIRTH|DOB)$', w)]):
+            dob = x
+            dob = dob.split(':')[1].strip()
+    
+    # Converting the extracted informaton into json
+    data = {
+        'Document Number':voter_no,
+        'Name':full_name,
+        'Father Name':elder_name,
+        'Sex':sex,
+        'DOB':dob,
+        'Document Type': "Voter ID"
+    }
+    return data
+def findword(textlist, wordstring):
+    lineno = -1
+    for wordline in textlist:
+        xx = wordline.split()
+        if ([w for w in xx if re.search(wordstring, w)]):
+            lineno = textlist.index(wordline)
+            textlist = textlist[lineno+1:]
+            return textlist
+    return textlist
+# DRIVERS LICENCE
+def Driving_Licence_read(text):
+    
+    name = None
+    add = None
+    dob = None
+    did = None
+    
+    nameline = []
+    addline = []
+    dobline = []
+    didline =[]
+    lines = text.split('\n')
+    for lin in lines:
+        s = lin.strip()
+        s = lin.replace('\n','')
+        s = s.rstrip()
+        s = s.lstrip()
+        text1.append(s)
+    text1 = list(filter(None, text1))
+    lineno = 0
+    for wordline in text1:
+            xx = wordline.split('\n')
+            if ([w for w in xx if re.search('(DRIVING LICENSE|DRIVER|DRIVE|DIVE|RIVE|DRVE|DRIE|DRIV|TRANSPORT|STATE|DRI|IVE|LICENCE|LICEN)$', w)]):
+                text1 = list(text1)
+                lineno = text1.index(wordline)
+                break
+    text0 = text1[lineno+1:]
+    try:
+        #cleaning number
+        text0 = findword(text1, '(Number|umber|Vehicle|N.|Vehi|Transport|Num)$')
+        didline = text0[0]
+        did = didline.rstrip()
+        did = did.lstrip()
+        did = did.replace(" ", "")
+        did = did.replace("\"", "")
+        did = did.replace(";", "")
+        did = did.replace("%", "L")
+        #cleaning name
+        name = text0[0]
+        name = name.rstrip()
+        name = name.lstrip()
+        name = name.replace("8", "B")
+        name = name.replace("0", "D")
+        name = name.replace("6", "G")
+        name = name.replace("1", "I")
+        name = re.sub('[^a-zA-Z] +', ' ', name)
+        #cleaning address
+        text0 = findword(text1, ('Address|Adress|ddress|Addess|Addrss|Addres|Add|Ad|Location)$'))
+        addline = text0[0]
+        add = addline.rstrip()
+        add = add.lstrip()
+        add = add.replace(" ", "")
+        add = add.replace("\"", "")
+        add = add.replace(";", "")
+        add = add.replace("%", "L")
+        # Cleaning DOB
+        dob = text0[2][:10]
+        dob = dob.rstrip()
+        dob = dob.lstrip()
+        dob = dob.replace('l', '/')
+        dob = dob.replace('L', '/')
+        dob = dob.replace('I', '/')
+        dob = dob.replace('i', '/')
+        dob = dob.replace('|', '/')
+        dob = dob.replace('\"', '/1')
+        dob = dob.replace(" ", "")
+    except:
+        pass
+    data = {}
+    data['Name'] = name
+    data['Address'] = add
+    data['Document Number'] = did
+    data['DOB'] = dob
+    data['Document Type'] = "Driving License"
+    
+def findword(textlist, wordstring):
+    lineno = -1
+    for wordline in textlist:
+        xx = wordline.split( )
+        if ([w for w in xx if re.search(wordstring, w)]):
+            lineno = textlist.index(wordline)
+            textlist = textlist[lineno+1:]
+            return textlist
+    return textlist
+
+#####
 
 def pdf2img(file_path: str):
     try:
---
+++ helpers/inference.py
@@ -0,0 +1,24 @@
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+
+classes_dict = {0: 'Driving License', 1: 'PAN', 2: 'Voter ID Card', 3: 'Adhaar Card', 4: 'Adhaar Card', 5: 'Adhaar Card'}
+
+def classify_image(model, device, image):
+    image = image.convert('RGB')
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.6544, 0.6596, 0.6643], std=[0.1878, 0.1899, 0.1975]),
+    ])
+    input_tensor = transform(image).unsqueeze(0).to(device)
+
+    with torch.no_grad():
+        output = model(input_tensor)
+    probabilities = torch.nn.functional.softmax(output[0], dim=0)
+    predicted_class = torch.argmax(probabilities).item()
+
+    # print(f"Predicted class: {predicted_class}")
+    # print(f"Probabilities: {probabilities}")
+    # print(f"Class name: {classes_dict[predicted_class]}")
+    return classes_dict[predicted_class]
